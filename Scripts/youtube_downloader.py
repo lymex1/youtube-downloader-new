ea1291fbc6fa067509da2d866528a626003adcb7
lymex1 <ekarinkin@gmail.com>
2025-07-31 19:58:03 +0300

    Add threading

--- Scripts/youtube_downloader.py
+++ Scripts/youtube_downloader.py
@@ -9,10 +9,6 @@ def download_youtube_video(url: str, output_path: str, cookie_file_path: str):
     output_path = edit_arr[0]
     
     
-    path_to_file = "media/video.mp4"
-    if os.path.exists(path_to_file):
-        os.remove(path_to_file)
-    
     ydl_opts = {
         'format': 'bestvideo[ext=mp4][height<=1080]+bestaudio[ext=m4a]/best[ext=mp4][height<=1080]',  # видео 1080p + аудио
         'outtmpl': output_path,
--- main/views.py
+++ main/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.views import View
 from django.conf import settings
 import os
-
+import uuid
+import threading
+from django.http import FileResponse
 from Scripts.youtube_downloader import download_youtube_video
 from YouTubeDownload.settings import BASE_DIR
 
@@ -24,10 +26,33 @@ class YouTubeProcessingView(View):
         if not url:
             return redirect("main:link_form")
 
-        output_path = os.path.join(settings.MEDIA_ROOT, "video.mp4")
+        temp_dir = os.path.join(settings.MEDIA_ROOT, "tmp")
+        os.makedirs(temp_dir, exist_ok=True)
+
+        # Уникальное имя
+        filename = f"{uuid.uuid4().hex}.mp4"
+        output_path = os.path.join(temp_dir, filename)
 
         try:
-            download_youtube_video(url, output_path, f"{BASE_DIR}/cookies.txt")
-            return render(request, "main/processing.html", {"ready": True, "filename": "video.mp4"})
+            # Скачиваем
+            download_youtube_video(url, output_path, "cookies.txt")
+
+            # Готовим ответ
+            response = FileResponse(open(output_path, 'rb'), content_type='video/mp4')
+            response['Content-Disposition'] = f'attachment; filename="video.mp4"'
+
+            # Удаление через 60 сек.
+            def delete_file(path):
+                try:
+                    os.remove(path)
+                    print(f"Удалён файл: {path}")
+                except Exception as e:
+                    print(f"Ошибка при удалении: {e}")
+
+            threading.Timer(60, delete_file, args=[output_path]).start()
+
+            return response
+
         except Exception as e:
-            return render(request, "main/processing.html", {"error": str(e)})
+            return redirect("main:link_form")  # или показать ошибку
+
