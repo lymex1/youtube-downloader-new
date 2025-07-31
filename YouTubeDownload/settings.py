5a8087866b8d7b8d323812ffe1754575e80f5fb8
lymex1 <ekarinkin@gmail.com>
2025-07-31 18:52:05 +0300

    Update

--- YouTubeDownload/settings.py
+++ YouTubeDownload/settings.py
@@ -1,21 +1,15 @@
 from pathlib import Path
 import os
+from dotenv import load_dotenv
+import dj_database_url
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-+-@!p=c6^22azmtoklm1gb_+=0nlisapxj!4b2)ei7h5ge!-#f'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+load_dotenv()
 
 # Application definition
 
@@ -65,10 +59,7 @@ WSGI_APPLICATION = 'YouTubeDownload.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
